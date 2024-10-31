729634073041845d9f7b60ecd85bf82d91e6a1fa
Pullata Harshavardhan <Pullata.Harshavardhan@ingrammicro.com>
2024-10-31 14:53:27 +0530

    New Changes

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace DataTypes
+﻿namespace Arrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To DataTypes");
+            Console.WriteLine("Welcome To Arrays");
         }
     }
 }
